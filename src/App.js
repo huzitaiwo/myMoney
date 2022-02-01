2e2f7bc5dc39552f64e9e2ec7d5d7dec30967cb0
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-01 22:34:04 +0100

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 
 import './App.css'
 
@@ -6,6 +6,19 @@ function App() {
   return (
     <div className="App">
       app component
+      <BrowserRouter>
+        <Switch>
+          <Route exact to="/">
+            <Home />
+          </Route>
+          <Route exact to="/signup">
+            <Signup />
+          </Route>
+          <Route exact to="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
