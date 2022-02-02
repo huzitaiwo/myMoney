923e5c61eb3439cc98a4b7d824ba22045847ddb4
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2022-02-02 01:07:42 +0100

    created handleSubmit function

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
+// styles
 import styles from './Signup.module.css';
 
 export default function Signup() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [displayName, setDisplayName] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(email, password)
+  }
 
   return (
     <form onSubmit={handleSubmit} className={styles['signup-form']}>
