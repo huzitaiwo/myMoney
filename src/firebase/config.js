38924afd17f8dcf8c7e71754c8dfa142690e8517
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-20 04:39:26 +0100

    connected with firebase-firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdxPLv3CGA4h5yAIzUG2Jkct_Yo50PzbM",
+  authDomain: "mymoney-fef13.firebaseapp.com",
+  projectId: "mymoney-fef13",
+  storageBucket: "mymoney-fef13.appspot.com",
+  messagingSenderId: "268441786201",
+  appId: "1:268441786201:web:7c6740cfcbeab0888a5e48"
+}
+
+// firebase init
+firebase.initializeApp(firebaseConfig)
+
+// init service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
