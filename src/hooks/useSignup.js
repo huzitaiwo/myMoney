0dd2d00538fe0bcc5cbbf715fbd9430654fdf4bc
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-20 04:49:03 +0100

    created error and isLoading state

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,2 +1,7 @@
 import { useState } from "react"
 import { projectAuth } from "../firebase/config"
+
+export const useSignup = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+}
