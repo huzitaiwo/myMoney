7707f331f6ad81337aa15657f87ea2ea1da76a28
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2022-02-21 05:21:03 +0100

    creating singup function

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -6,7 +6,18 @@ export const useSignup = () => {
   const [error, setError] = useState(null)
 
   const singup = (email, password, displayName) => {
-    
+    setError(null)
+    setIsLoading(tru)
+
+    try {
+      // singup user
+      const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+    }
+    catch(err) {
+      console.log(err.message)
+      setError(err.message)
+      setIsLoading(false)
+    }
   }
 
   return { isLoading, error, singup }
