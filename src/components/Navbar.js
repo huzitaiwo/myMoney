10dfa4eadbcd247f24051c51154438a154fa77a2
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-19 18:23:06 +0100

    Link to homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}>myMoney</li>
+        <li className={styles.title}><Link to='/'>myMoney</Link></li>
 
         {!user && 
           <>
