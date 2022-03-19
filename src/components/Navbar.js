9587417844ff6bc3041fb3dc327e4689733f3953
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-19 18:17:29 +0100

    used user from useAuthContext hook for Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,13 +13,22 @@ export default function Navbar() {
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
-        
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
 
-        <li>
-          <button className='btn' onClick={logout}>Logout</button>
-        </li>
+        {!user && 
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        }
+
+        {user &&
+          <>
+            <p>HI {user.displayName}</p>
+            <li>
+              <button className='btn' onClick={logout}>Logout</button>
+            </li>
+          </>
+        }
       </ul>
     </nav>
   )
