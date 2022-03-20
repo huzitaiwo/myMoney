ff8d276b058aba1fff617138dea4118ba92f0dfe
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-20 23:57:10 +0100

    creating Transaction form

---
+++ src/pages/home/TransctionForm.js
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+
+export default function TransctionForm() {
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState('')
+
+  return (
+    <div>TransctionForm</div>
+  )
+}
