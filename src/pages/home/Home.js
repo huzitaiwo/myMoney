5657d623f8f190025f0fbe4b4a3d43b8d0d84608
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-20 23:55:37 +0100

    Homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,15 @@
-// import styles from './Home.module.css'
+import styles from './Home.module.css'
 
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.content}>
+        transaction list
+      </div>
+      <div className={styles.sidebar}>
+        
+      </div>
     </div>
   )
 }
