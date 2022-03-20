1ddedf20f1808bbb777f3f30bb6363098ae2e914
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-21 00:13:54 +0100

    creating useFirestore hook

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,6 +1,20 @@
 import { useReducer, useEffect, useState } from 'react'
 import { projectFirestore } from '../firebase/config'
 
-export const useFirestore = () => {
+let initialState = {
+  document: null,
+  error: null,
+  isLoading: false,
+  success: null
+}
 
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export const useFirestore = collection => {
+  const [reponse, dispatch] = useReducer(firestoreReducer, initialState)
 }
