875e99f71016224bc1fa4a71fb4a4398432db05b
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-21 00:01:45 +0100

    created Transaction form

--- src/pages/home/TransctionForm.js
+++ src/pages/home/TransctionForm.js
@@ -5,10 +5,15 @@ export default function TransctionForm() {
   const [name, setName] = useState('')
   const [amount, setAmount] = useState('')
 
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log({ name, amount})
+  }
+
   return (
     <>
       <h3>Add a Transaction</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <span>Transation name:</span>
           <input 
@@ -18,6 +23,16 @@ export default function TransctionForm() {
             value={name}
           />
         </label>
+        <label>
+          <span>Transation amount:</span>
+          <input 
+            type="number"
+            required
+            onChange={e => setAmount(e.target.value)}
+            value={amount}
+          />
+        </label>
+        <button>Add Transaction</button>
       </form>
     </>
   )
