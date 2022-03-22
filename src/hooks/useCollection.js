7cd534cfbc67f865d68e57af54c02a2103b4aa4c
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-22 14:50:13 +0100

    creating useCollection hook

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,8 +1,19 @@
-import { useReducer, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { projectFirestore, timestamp } from '../firebase/config'
 
-export const useCollection = () => {
+export const useCollection = collecction => {
   const [documents, setDocuments] = useState(null)
   const [error, setError] = useState(null)
+
+  useEffect(() => {
+    let ref = projectFirestore.collection(collecction)
+
+    ref.onSnapshot(snapshot => {
+      let results = []
+      snapshot.docs.forEach(doc => {
+        results.push({ ...doc.data(), id: doc.id })
+      })
+    })
+  }, [])
   
 }
