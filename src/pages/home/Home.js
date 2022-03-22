4bf805b950477512f7338a2c1c29367630ee2287
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-22 14:56:16 +0100

    using useCollection hook

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import { useAuthContext } from "../../hooks/useAuthContext"
+import { useCollection } from "../../hooks/useCollection"
 
 // styles
 import styles from './Home.module.css'
@@ -9,6 +10,7 @@ import TransctionForm from './TransctionForm'
 
 export default function Home() {
   const { user } = useAuthContext()
+  const { documents, error} = useCollection('transactions')
 
   return (
     <div className={styles.container}>
