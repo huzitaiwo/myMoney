6eadbdb0718619012d53fb829b3cdc71f2dbaaaf
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-23 15:55:27 +0100

    cleanup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -29,8 +29,6 @@ export function AuthContextProvider({ children }) {
     })
   }, [])
 
-  console.log('AuthContext state:', state)
-
   return(
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
